c6c7acbc09784a83b9a1299c39904aaf5e90a377
Desh Deepak Kant <118960904+DeshDeepakKant@users.noreply.github.com>
2024-06-21 17:53:07 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (97.09%) - LeetSync

---
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+
+        string s = "";
+        int i = 0;
+        while (str1[i] == str2[i] && i < gcd(str1.length(), str2.length())) {
+            s.push_back(str1[i]);
+            i++;
+        }
+        int sl = s.size();
+        if (!sl || (str1.length() % sl) ||(str2.length() % sl)) {
+            return "";
+        }
+        for (int i = 0; i < str1.length(); i++) {
+            if (s[i % sl] != str1[i]) {
+                return "";
+            }
+        }
+        for (int i = 0; i < str2.length(); i++) {
+            if (s[i % sl] != str2[i]) {
+                return "";
+            }
+        }
+        return s;
+    }
+};
