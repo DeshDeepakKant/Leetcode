f5d8782e21e325cb3d28f6c112f5e6e26e273456
Desh Deepak Kant <118960904+DeshDeepakKant@users.noreply.github.com>
2024-06-27 17:23:21 +0530

    Time: 44 ms (9.98%) | Memory: 21.3 MB (91.64%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool check(long long mid, vector<int>& piles,int h) {
+        long long ans = 0;
+        for (auto it : piles) {
+            ans += ceil((1.0 * it) / mid);
+        }
+        if (h >= ans) {
+            return true;
+        }
+        return false;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+
+        long long l = 1, r = 1e9;
+        while (l <= r) {
+            long long mid = (l + r) / 2;
+            if (check(mid, piles, h)) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
+};
