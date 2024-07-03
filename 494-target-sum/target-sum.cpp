d4dd4152ac44d0c6229c80173f3ed5c1dfdf7d7b
Desh Deepak Kant <118960904+DeshDeepakKant@users.noreply.github.com>
2024-07-03 15:40:42 +0530

    Time: 9 ms (62.73%) | Memory: 13.6 MB (57.07%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,23 +1,34 @@
 class Solution {
 public:
-    int ans=0;
-    void f(int i,vector<int>& v,int t,int s)
-    {
-       if(i<0)
-       {
-         if(s==t)
-         {
-            ans++;
-         }
-         return;
-       }
-       f(i-1,v,t,s+v[i]);
-       f(i-1,v,t,s-v[i]);
-       return;
-       
-    }
     int findTargetSumWays(vector<int>& nums, int target) {
-        f(nums.size()-1,nums,target,0);
-        return ans;
+        int n = nums.size();
+        int s = accumulate(nums.begin(), nums.end(), 0);
+        if ((s - target) % 2 || (s < target)) {
+            return 0;
+        }
+        s = (s - target) / 2;
+        vector<vector<int>> dp(n, vector<int>(s + 1, 0));
+        if(nums[0]==0)
+        {
+            dp[0][0]=2;
+        }
+        else
+        {
+            dp[0][0]=1;
+        }
+        if (nums[0] != 0 && nums[0] <= s)
+         dp[0][nums[0]] = 1;
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j <= s; j++) {
+                int a = dp[i - 1][j];
+                int b = 0;
+                if (j >= nums[i]) {
+                    b = dp[i - 1][j - nums[i]];
+                }
+                dp[i][j]= a + b;
+            }
+        }
+        return dp[n - 1][s];
     }
 };
